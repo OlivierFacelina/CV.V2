f24f206bf6d04d8c2616bc60c31e8c9a487d7e5b
OlivierFacelina <olivier.facelina@gmail.com>
2024-05-22 17:01:46 +0200

    🚧 modal done but informations is missing

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+function openModal(index) {
+  document.getElementById('modal-' + index).style.display = 'block';
+}
+
+function closeModal(index) {
+  document.getElementById('modal-' + index).style.display = 'none';
+}
