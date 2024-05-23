8eb9961fd30f355252d98b03998dbd29c28b66a1
OlivierFacelina <olivier.facelina@gmail.com>
2024-05-23 15:16:19 +0200

    ✨ projects done

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,27 @@
+// ------------------------------ PROJECTS ------------------------------
+
 function openModal(index) {
   document.getElementById('modal-' + index).style.display = 'block';
 }
 
 function closeModal(index) {
   document.getElementById('modal-' + index).style.display = 'none';
+}
+
+function showMore() {
+  document.querySelectorAll('.project-button.hidden').forEach(button => {
+      button.classList.remove('hidden');
+  });
+  document.getElementById('see-more-button').classList.add('hidden');
+  document.getElementById('see-less-button').classList.remove('hidden');
+}
+
+function showLess() {
+  document.querySelectorAll('.project-button').forEach((button, index) => {
+      if (index >= 12) {
+          button.classList.add('hidden');
+      }
+  });
+  document.getElementById('see-more-button').classList.remove('hidden');
+  document.getElementById('see-less-button').classList.add('hidden');
 }
