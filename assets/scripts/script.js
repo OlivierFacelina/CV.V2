9bc41e9116a9c4f3f4969a27eb30ef869fb57957
OlivierFacelina <olivier.facelina@gmail.com>
2024-05-23 16:56:00 +0200

    🚧 section 1 (sports)

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -25,3 +25,47 @@ function showLess() {
   document.getElementById('see-more-button').classList.remove('hidden');
   document.getElementById('see-less-button').classList.add('hidden');
 }
+
+// ------------------------------ HOBBIES ------------------------------
+
+// ------------ SPORTS DATA ---------------
+const sportsData = {
+  labels: ["Tennis", "Football", "Basketball", "Formule 1", "Hockey", "Football américain"],
+  data: [35,30,15,15,2.5,2.5],
+  colors: ["#3A9D23", "#000080", "#FF804E", "#FF1E04", "orange", "#724144"]
+}
+const sportsChart = document.querySelector('.sports-chart');
+const ul = document.querySelector(".hobbies__content .details ul")
+
+new Chart(sportsChart, {
+  type: "doughnut",
+  data: {
+    labels: sportsData.labels,
+    datasets: [
+      {
+        label: "Loisirs de prédilection",
+        data: sportsData.data,
+        backgroundColor: sportsData.colors
+      }
+    ]
+  },
+  options: {
+    // borderWidth: 10,
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  }
+
+})
+
+const populateUl = () => {
+  sportsData.labels.forEach((l, i) => {
+    let li = document.createElement("li");
+    li.innerHTML = `${l}: <span class='percentage'>${sportsData.data[i]}%</span>`;
+    ul.appendChild(li);
+  })
+}
+
+populateUl();
